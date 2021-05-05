51ac2ee47800976fcee32758bca79f7a57ecfdb7
isXander <43245524+isXander@users.noreply.github.com>
2021-05-05 19:36:42 +0100

    Base

---
+++ src/main/java/co/uk/isxander/capetest/CapeTest.java
@@ -0,0 +1,12 @@
+package co.uk.isxander.capetest;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = CapeTest.MOD_ID, name = CapeTest.MOD_NAME, version = CapeTest.MOD_VERSION, clientSideOnly = true)
+public class CapeTest {
+
+    public static final String MOD_NAME = "isXander's Cape Test";
+    public static final String MOD_ID = "capetest";
+    public static final String MOD_VERSION = "1.0";
+
+}
